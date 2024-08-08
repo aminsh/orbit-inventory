407261be4c1a61461a5968e55ce297b097fd3ff2
aminsh <amin_s@felixin.io>
2024-08-08 23:54:52 +0330

    style(app): remove old auth

--- frontend/packages/auth/src/config/index.ts
+++
@@ -1,5 +0,0 @@
-import { useMemory } from '@orbit/core'
-import { translateDictionary } from './translate.dictionary'
-import { configure } from '@testing-library/react'
-
-useMemory()
--- frontend/packages/auth/src/constant.ts
+++
@@ -1,6 +0,0 @@
-export const AUTHENTICATION_TOKEN = 'AUTHENTICATION_TOKEN'
-export const CALLBACK_URL = 'callbackUrl'
-export const ACCESS_TOKEN = 'access_token'
-export const TOKEN_TYPE = 'token_type'
-export const ORIGINAL_PATH = 'originalPath'
-export const DEFAULT_PATH = '/'
